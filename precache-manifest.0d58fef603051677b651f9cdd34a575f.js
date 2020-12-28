d061d3e6fbde7eff3e1bf824cf709a74e0758633
Ravikant Khandelwal <ravikant.khandelwal@bts.com>
2020-12-28 12:06:39 +0530

    Updates

---
+++ precache-manifest.0d58fef603051677b651f9cdd34a575f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c3153444d6369b80622d8824c9b820",
+    "url": "/react-menu/index.html"
+  },
+  {
+    "revision": "4d97edbe418e560ae6b4",
+    "url": "/react-menu/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/react-menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d97edbe418e560ae6b4",
+    "url": "/react-menu/static/js/main.11039ec5.chunk.js"
+  },
+  {
+    "revision": "16969508fe17c64f3354",
+    "url": "/react-menu/static/js/runtime-main.a64c297e.js"
+  }
+]);
